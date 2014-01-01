afaa1f1ba201139c164ea606c8a4c60281f932e7
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-01-01 12:04:02 +0100

    Bump year

--- lib/App/perluse.pm
+++ lib/App/perluse.pm
@@ -35,7 +35,7 @@ Piotr Roszatycki <dexter@cpan.org>
 
 =head1 LICENSE
 
-Copyright (c) 2011-2013 Piotr Roszatycki <dexter@cpan.org>.
+Copyright (c) 2011-2014 Piotr Roszatycki <dexter@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as perl itself.
